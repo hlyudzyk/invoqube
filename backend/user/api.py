0f2b973f371cbc1ae9a1112323af18ea293a2dd5
Erik Slanc <erikslanc@Emas-MacBook-Pro-Tab-2.local>
2025-12-03 23:25:24 +0100

    feat: add custom auth serializers and JWT token handling

--- backend/user/api.py
+++ backend/user/api.py
@@ -1,5 +1,7 @@
 from django.http import JsonResponse
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from user.models import User
 from user.serializers import UserDetailSerializer
@@ -12,3 +14,31 @@ def user_detail(request, pk):
   user = User.objects.get(pk=pk)
   serializer = UserDetailSerializer(user,many=False)
   return JsonResponse(serializer.data,safe=False)
+
+
+@api_view(['POST'])
+@authentication_classes([JWTAuthentication])
+@permission_classes([IsAuthenticated])
+def edit_account(request):
+  user = request.user
+  
+  # Update user fields
+  user.name = request.data.get('name', user.name)
+  user.description = request.data.get('description', user.description)
+  user.business_name = request.data.get('business_name', user.business_name)
+  user.vat_number = request.data.get('vat_number', user.vat_number)
+  user.registration_number = request.data.get('registration_number', user.registration_number)
+  user.address = request.data.get('address', user.address)
+  user.city = request.data.get('city', user.city)
+  user.postal_code = request.data.get('postal_code', user.postal_code)
+  user.country = request.data.get('country', user.country)
+  user.phone = request.data.get('phone', user.phone)
+  
+  # Handle avatar upload
+  if 'avatar' in request.FILES:
+    user.avatar = request.FILES['avatar']
+  
+  user.save()
+  
+  serializer = UserDetailSerializer(user, many=False)
+  return JsonResponse(serializer.data, safe=False)
